400e4065427b7116e1b26c9128a82cd2338bb1ed
Tobias Schoofs <tobias.schoofs@gmx.net>
2023-03-04 13:00:15 +0000

    [dijkstra-review] two more test cases

--- misc/test/suite/Spec.hs
+++ misc/test/suite/Spec.hs
@@ -1,11 +1,12 @@
 import Test.Hspec
 
 import Data.Functor ((<$>))
+import Control.Exception (evaluate)
 
 import Algorithms.Dijkstra
 
 main :: IO ()
-main = hspec $ do
+main = hspec $
   describe "Dijkstra" $ do
 
     it "finds the shortest path" $ do
@@ -40,6 +41,17 @@ main = hspec $ do
       let r = buildRepSimple
       nodVal <$> astar noend dst heu r 1 `shouldBe` []
 
+    it "terminates when s does not exist" $ do
+      let r = buildRepSimple
+      nodVal <$> astar end dst heu r 10 `shouldBe` []
+
+    it "terminates when s = d" $ do
+      let r = buildRepSimple
+      nodVal <$> astar start dst heu r 1 `shouldBe` [1]
+
+    it "throws exception when graph is broken" $ do
+      let r = buildRepBroken
+      evaluate (astar end dst heu r 1) `shouldThrow` anyException
 
 makeNodes :: [Node Int]
 makeNodes = [ N 1 0 [] False Nothing
@@ -56,7 +68,7 @@ makeNodes = [ N 1 0 [] False Nothing
 buildRepSimple :: Rep Int
 buildRepSimple = case makeNodes of
                    [a,b,c,d,e,f,g,h,i] ->
-                     let r1 = initNodes $ [a,b,c,d,e,f,g,h,i]
+                     let r1 = initNodes [a,b,c,d,e,f,g,h,i]
                          r2 = addNeis r1 a [2,3]
                          r3 = addNeis r2 b [4]
                          r4 = addNeis r3 c [5]
@@ -118,6 +130,19 @@ buildRepWithCycle2 = case makeNodes of
                               r8 = addNeis r7 g [8]
                            in addNeis r8 h [9]
 
+buildRepBroken :: Rep Int
+buildRepBroken = case makeNodes of
+                   [a,b,c,d,e,f,g,h,i] ->
+                      let r1 = initNodes [a,b,c,d,e,f,g,h,i]
+                          r2 = addNeis r1 a [3,2]
+                          r3 = addNeis r2 b [40]
+                          r4 = addNeis r3 c [50]
+                          r5 = addNeis r4 d [60]
+                          r6 = addNeis r5 e [10]
+                          r7 = addNeis r6 f [70]
+                          r8 = addNeis r7 g [80]
+                       in addNeis r8 h [9]
+
 buildRepWithoutPath :: Rep Int
 buildRepWithoutPath = initNodes makeNodes
 
@@ -140,6 +165,10 @@ end _             = False
 
 noend :: Node Int -> Bool
 noend _ = False
+
+start :: Node Int -> Bool
+start (N 1 _ _ _ _) = True
+start _             = False
 
 dst :: Node Int -> Node Int -> Integer
 dst _ _ = 1
